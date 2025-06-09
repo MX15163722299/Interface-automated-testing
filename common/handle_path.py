839d01a2f3a5621c6835c29222d2001db3d96baf
peter <784007352@qq.com>
2025-06-09 12:43:59 +0800

    更新了包和模块的命名规范

--- common/handle_path.py
+++ common/handle_path.py
@@ -3,7 +3,7 @@ import os
 # DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DIR = os.path.dirname(os.path.dirname(__file__))
 CONFIG_DIR = DIR
-CASE_DIR = os.path.join(DIR, 'testcase_123')
+CASE_DIR = os.path.join(DIR, 'testcase')
 DATA_DIR = os.path.join(DIR, 'testdata')
 LOG_DIR = os.path.join(DIR, 'testlog')
 REPORT_DIR = os.path.join(DIR, 'testreport')
